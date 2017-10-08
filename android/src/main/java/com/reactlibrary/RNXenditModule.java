e32c622bc6f78c3dca12b066d4e7fa54fc8bc490
Tirta Wirya Putra <tirtawiryaputra@yahoo.com>
2017-10-08 15:52:29 +0700

    :wrench: FIXING | return

--- android/src/main/java/com/reactlibrary/RNXenditModule.java
+++ android/src/main/java/com/reactlibrary/RNXenditModule.java
@@ -22,6 +22,6 @@ public class RNXenditModule extends ReactContextBaseJavaModule {
 
   @ReactMethod
   public void startScan() {
-    Log.e(REACT_CLASS, "start scan Xendit..");
+    return ("start scan Xendit..");
   }
 }
